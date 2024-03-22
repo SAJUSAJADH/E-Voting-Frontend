0a57b5d43dd58d6f1794e51b3d60e709777ff7c9
sajadh <sajadh2001s@gmail.com>
2024-03-22 16:52:28 +0530

    smart contract update

--- binaryData/constant.js
+++ binaryData/constant.js
@@ -1,8 +1,8 @@
 import Electionabi from './Election.json'
-import Electionfactabi from './Electionfact.json'
+import Electionfactabi from './ElectionFact.json'
 
 const ElectionABI = Electionabi.abi
 const ElectionFactABI = Electionfactabi.abi
-const contractAddress = '0x0e0573edE4C4E09088dda211c72c87A4bE6aBF51'
+const contractAddress = '0x778109394f5A076215f0aF541311c3A660C21CBe'
 
 export { ElectionABI, ElectionFactABI, contractAddress }
